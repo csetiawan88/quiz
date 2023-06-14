63d7569747556d0f70a8793460aa048aa6319348
csetiawan88 <csetiawan@yahoo.com>
2023-06-14 11:36:01 +1000

    Edit Questions

---
+++ assets/question.js
@@ -0,0 +1,72 @@
+// Questions. SRC:https://www.sanfoundry.com/1000-javascript-questions-answers/
+let questions = [
+  {
+    q: "Which of the following is not an error in JavaScript?",
+    choices: [
+      { choice: "1. Missing of Bracket" },
+      { choice: "2. Division by zero" },
+      { choice: "3. Syntax error" },
+      { choice: "4. Missing of semicolons" },
+    ],
+    a: "2. Division by zero",
+  },
+  {
+    q: "Why JavaScript Engine is needed?",
+    choices: [
+      { choice: "1. Both Compiling & Interpreting the JavaScript" },
+      { choice: "2. Parsing the javascript" },
+      { choice: "3. Interpreting the JavaScript" },
+      { choice: "4. Compiling the JavaScript" },
+    ],
+    a: "3. Interpreting the JavaScript",
+  },
+  {
+    q: "What is JavaScript",
+    choices: [
+      {
+        choice:
+          "1. JavaScript is a scripting language used to make the website interactive",
+      },
+      {
+        choice:
+          "2. JavaScript is an assembly language used to make the website interactive",
+      },
+      {
+        choice:
+          "3.  JavaScript is a compiled language used to make the website interactive",
+      },
+      { choice: "4. None of the mentioned" },
+    ],
+    a: "1. JavaScript is a scripting language used to make the website interactive",
+  },
+  {
+    q: "Which of the following is correct about JavaScript?",
+    choices: [
+      { choice: "1. JavaScript is an Object-Based language" },
+      { choice: "2. JavaScript is Assembly-language" },
+      { choice: "3. JavaScript is an Object-Oriented language" },
+      { choice: "4. JavaScript is a High-level language" },
+    ],
+    a: "1. JavaScript is an Object-Based language",
+  },
+  {
+    q: "Arrays in JavaScript are defined by which of the following statements?",
+    choices: [
+      { choice: "1. It is an ordered list of values" },
+      { choice: "2. It is an ordered list of objects" },
+      { choice: "3. It is an ordered list of string" },
+      { choice: "4. It is an ordered list of functions" },
+    ],
+    a: "1. It is an ordered list of values",
+  },
+  {
+    q: "Which of the following is not javascript data types?",
+    choices: [
+      { choice: "1. Null type" },
+      { choice: "2. Undefined type" },
+      { choice: "3. Number type" },
+      { choice: "4. All of the mentioned" },
+    ],
+    a: "4. All of the mentioned",
+  },
+];
